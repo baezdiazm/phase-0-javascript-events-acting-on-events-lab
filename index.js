9b5fad8c24f8f72d1e664dd38918dbe772875935
baezdiazm <baezdiazm@yahoo.com>
2021-11-17 17:36:08 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 2080) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'leftArrow') {
+        moveDodgerLeft()
+    }
+    else if (event.key === 'arrowRight') {
+        moveDodgerRight()
+    }
+  }
+  )
